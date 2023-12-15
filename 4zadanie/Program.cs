e4e17ba8393da8c9f1cf4a83257bbd05f6a04ac5
clown <artemkoka@bk.ru>
2023-12-15 16:26:53 +0300

    Добавьте файлы проекта.

---
+++ 4zadanie/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4zadanie
+{
+    internal class Program
+    {
+        public string[] SplitLine(string line)
+        {
+            string[] subs = line.Split(' ');
+            return subs;
+        }
+        
+        public void PrintLines(string[] words)
+        {
+            foreach (string s in words)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение с пробелом после каждого слова");
+            string line = Console.ReadLine();
+            Program program = new Program();
+            string[] subs = program.SplitLine(line);
+            program.PrintLines(subs);
+        }
+    }
+}
